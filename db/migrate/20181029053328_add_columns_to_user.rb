64c3f09ce573f39dc681b56aa0a5f810c52211ae
Athoi <athoi@Athois-MacBook-Pro.local>
2018-10-29 16:37:50 +1100

    First commit, generated controller and installed devise

---
+++ db/migrate/20181029053328_add_columns_to_user.rb
@@ -0,0 +1,14 @@
+class AddColumnsToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :phone, :string
+    add_column :users, :street, :string
+    add_column :users, :city, :string
+    add_column :users, :state, :string
+    add_column :users, :postcode, :string
+    add_column :users, :country, :string, :default => "Australia"
+    add_column :users, :longitude, :float
+    add_column :users, :latitude, :float
+  end
+end
