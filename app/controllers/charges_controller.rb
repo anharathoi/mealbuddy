a926c32a1a8af4635ff304322002a9d269c93b3b
Luke Stainlay <lukestainlay@gmail.com>
2018-11-02 10:17:22 +1100

    changing order creation

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -16,6 +16,9 @@ class ChargesController < ApplicationController
         :currency    => 'aud'
       )
     
+
+
+      
     rescue Stripe::CardError => e
       flash[:error] = e.message
       redirect_to new_charge_path
