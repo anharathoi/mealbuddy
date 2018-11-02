fa5b443a2da90d92ba5fd8bd3e929e046ed423b7
Luke Stainlay <lukestainlay@gmail.com>
2018-11-02 14:28:04 +1100

    added paid column to Orders

---
+++ db/migrate/20181102032220_add_column_to_orders.rb
@@ -0,0 +1,5 @@
+class AddColumnToOrders < ActiveRecord::Migration[5.2]
+  def change
+    add_column :orders, :paid, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_31_020112) do
+ActiveRecord::Schema.define(version: 2018_11_02_032220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2018_10_31_020112) do
     t.float "latitude"
     t.float "longitude"
     t.string "title"
+    t.boolean "paid"
     t.index ["meal_id"], name: "index_orders_on_meal_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
