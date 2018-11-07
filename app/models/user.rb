403cdbe9c6881410bb98baa9dd0f0719971394e7
Athoi <athoi@Athois-MacBook-Pro.local>
2018-11-07 17:36:23 +1100

    If user deletes account, meals created by user are not available

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :meals
+  has_many :meals, dependent: :nullify
   has_many :orders, dependent: :nullify
 
   geocoded_by :address
